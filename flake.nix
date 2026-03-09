818827bccc001884a844a0ec0d1cf00c24f64834
Max Cabrajac <max@cabrajac.com>
2026-03-09 20:49:05 -0300

    Update nixcaps

--- flake.nix
+++ flake.nix
@@ -5,24 +5,34 @@
 		flake-utils.follows = "nixcaps/flake-utils";
 	};
 
-	outputs = {
+	outputs = inputs @ {
 		nixpkgs,
 		flake-utils,
-		nixcaps,
 		...
-	}: flake-utils.lib.eachDefaultSystem (
-			system: let
-				pkgs = nixpkgs.legacyPackages.${system};
-				compile = nixcaps.packages.${system}.compile;
-				lib = pkgs.lib;
+	}:
+		flake-utils.lib.eachDefaultSystem (system: let
+			pkgs = nixpkgs.legacyPackages.${system};
+			nixcaps = inputs.nixcaps.lib.${system};
+			redox = {
+				src = ./redox;
+				keyboard = "redox";
+				variant = "rev1";
+			};
+		in {
+			packages = {
+				redox = nixcaps.mkQmkFirmware redox;
+			};
 
-				forEachSubPackage = dir: f: let
-					subdirs = lib.filterAttrs (_: type: type == "directory") (builtins.readDir dir);
-					subpkgs = lib.mapAttrs (name: _: pkgs.callPackage (dir + "/${name}") { }) subdirs;
-				in
-					builtins.mapAttrs (_: pkg: f pkg) subpkgs;
-			in {
-				packages = forEachSubPackage ./. compile;
-			}
-		);
+			apps = {
+				redox = nixcaps.flashQmkFirmware redox;
+			};
+
+			devShells.default = pkgs.mkShell {
+				QMK_HOME = "${inputs.nixcaps.inputs.qmk_firmware}";
+				packages = [ pkgs.qmk ];
+				shellHook = ''
+					ln -sf "${nixcaps.mkCompileDb redox}/compile_commands.json" ./redox/compile_commands.json
+				'';
+			};
+		});
 }
