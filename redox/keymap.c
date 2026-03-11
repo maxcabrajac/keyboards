07c45793c34df8bc687a1cb6d018dd5b8ef77735
Max Cabrajac <max@cabrajac.com>
2026-03-11 19:01:52 -0300

    Change sudoku layer

--- redox/keymap.c
+++ redox/keymap.c
@@ -28,6 +28,7 @@ enum custom_keycodes {
 #define D_Gaming TG(_GAMING)
 #define D_RevGam TG(_GAMING_REVCOLEMAK)
 #define D_Sudoku TG(_SUDOKU)
+#define D_HldSdk MO(_SUDOKU)
 #define D_Num NUMB
 #define D_Nav MO(_NAV)
 
@@ -214,13 +215,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
 	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                                            KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,XXXXXXX ,XXXXXXX ,
 //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_7    ,KC_8    ,KC_9    ,XXXXXXX ,XXXXXXX ,
+	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_B    ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_7    ,KC_8    ,KC_9    ,XXXXXXX ,XXXXXXX ,
 //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	XXXXXXX ,XXXXXXX ,D_CTLSFT,KC_LSFT ,KC_LCTL ,XXXXXXX ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_4    ,KC_5    ,KC_6    ,KC_0    ,XXXXXXX ,
+	XXXXXXX ,XXXXXXX ,KC_V    ,KC_X    ,KC_C    ,KC_Z    ,XXXXXXX ,                          XXXXXXX ,XXXXXXX ,KC_4    ,KC_5    ,KC_6    ,KC_0    ,XXXXXXX ,
 //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,        D_Sudoku,XXXXXXX ,XXXXXXX ,KC_1    ,KC_2    ,KC_3    ,XXXXXXX ,XXXXXXX ,
+	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_LSFT ,XXXXXXX ,XXXXXXX ,XXXXXXX ,        D_Sudoku,XXXXXXX ,XXXXXXX ,KC_1    ,KC_2    ,KC_3    ,XXXXXXX ,XXXXXXX ,
 //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     KC_BSPC ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    XXXXXXX ,     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX
+	XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     KC_BSPC ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    XXXXXXX ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT
 //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
